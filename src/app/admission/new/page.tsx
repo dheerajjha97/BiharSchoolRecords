c08501059bc687de0c770a48907ea72d19111a77
Dheeraj Jha <jhadheeraj97@gmail.com>
2025-07-12 12:35:40 +0000

    और कोई कमी आपकी नजर में हो जरूरी हो या कोई अतिरिक्त चीज जो नहीं होनी चाह

--- src/app/admission/new/page.tsx
+++
@@ -1,7 +0,0 @@
-
-import { redirect } from 'next/navigation';
-
-export default function NewAdmissionPage() {
-  redirect('/form');
-  return null;
-}
--- src/app/dashboard/admissions/new/page.tsx
+++
@@ -1,7 +0,0 @@
-
-import { redirect } from 'next/navigation';
-
-export default function DeprecatedNewAdmissionPage() {
-  redirect('/form');
-  return null;
-}
