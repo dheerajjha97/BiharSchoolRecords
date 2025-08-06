a0598e52d9ef33792e9f5242862fd4d20e63610d
Dheeraj Jha <jhadheeraj97@gmail.com>
2025-08-06 11:58:49 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/next.config.ts
+++ src/next.config.ts
@@ -3,6 +3,19 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Cross-Origin-Opener-Policy',
+            value: 'same-origin-allow-popups',
+          },
+        ],
+      },
+    ];
+  },
   generateBuildId: async () => {
     // This adds a timestamp to the build ID, effectively busting the cache on each new build.
     return new Date().getTime().toString();
